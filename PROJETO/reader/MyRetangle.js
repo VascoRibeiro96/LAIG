ff0d97a8aacf1311ec8fe337ac4c471a8e32eb16
VascoRibeiro96 <vascovsc1996@gmail.com>
2016-10-21 22:04:59 +0100

    MyRetangle implementation commit

---
+++ PROJETO/reader/MyRetangle.js
@@ -0,0 +1,58 @@
+/**
+ * MyRetangle
+ * @constructor
+ */
+ function MyQuad(scene, x1, y1, x2, y2) {
+ 	CGFobject.call(this,scene);
+
+	x1 = typeof x1 !== 'undefined' ? x1 : 0.0;
+	x2 = typeof x2 !== 'undefined' ? x2 : 1.0;
+	y1 = typeof y1 !== 'undefined' ? y1 : 0.0;
+	y2 = typeof y2 !== 'undefined' ? y2 : 1.0;
+	this.x1 = x1;
+	this.x2 = x2;
+	this.y1 = y1;
+	this.y2 = y2;
+
+ 	this.initBuffers();
+ };
+
+ MyQuad.prototype = Object.create(CGFobject.prototype);
+ MyQuad.prototype.constructor = MyQuad;
+
+ MyQuad.prototype.initBuffers = function() {
+
+ 	this.vertices = [
+ 	x1, y1, 0,
+ 	x2, y1, 0,
+ 	x1, y2, 0,
+ 	x2, y2, 0
+ 	];
+
+ 	this.indices = [
+ 	0, 1, 2, 
+ 	3, 2, 1
+ 	];
+
+ 	this.primitiveType = this.scene.gl.TRIANGLES;
+
+    this.normals = [
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1
+    ];
+
+    this.texCoords = [
+
+	this.x2,this.y1,
+	this.x2,this.y2,
+	this.x1,this.y1,
+	this.x1,this.y2
+	
+	];
+    
+
+    
+ 	this.initGLBuffers();
+ };
